730c938b69660b121508e7dd0bb59f3a0eaaae85
Aj007ar <aj007.ar@gmail.com>
2022-08-16 17:41:55 +0530

    [Ajay] Added: UC1 Employee Attendance

--- EmpWageMultipleCompany/Program.cs
+++ EmpWageMultipleCompany/Program.cs
@@ -5,6 +5,22 @@
         static void Main(string[] args)
         {
             Console.WriteLine("\n****************WELCOME TO EMPLOYEE WAGE FOR MULTIPLE COMPANY****************");
+            CheckAttendance();
+        }
+        public static void CheckAttendance()
+        {
+            int Full_Time = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == 1)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
         }
     }
 }
