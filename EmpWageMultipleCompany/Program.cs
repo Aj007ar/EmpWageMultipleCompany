90806b24a4453a494cc5dcc67ee151387304f6ea
Aj007ar <aj007.ar@gmail.com>
2022-08-16 14:19:29 +0530

    [Ajay] Added: Employee wage for multiple company

---
+++ EmpWageMultipleCompany/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmpWageMultipleCompany
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n****************WELCOME TO EMPLOYEE WAGE FOR MULTIPLE COMPANY****************");
+        }
+    }
+}
