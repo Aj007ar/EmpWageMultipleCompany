a03fa0985def390f551219c2a31dfc1712d74158
Aj007ar <aj007.ar@gmail.com>
2022-08-16 17:55:13 +0530

    [Ajay] Added: UC2 Employee Daily Wage

--- EmpWageMultipleCompany/Program.cs
+++ EmpWageMultipleCompany/Program.cs
@@ -5,6 +5,26 @@
         static void Main(string[] args)
         {
             Console.WriteLine("\n****************WELCOME TO EMPLOYEE WAGE FOR MULTIPLE COMPANY****************");
+            DailyWage();
+        }
+        public static void DailyWage()
+        {
+            int Full_Time = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == 1)
+            {
+                int empRatePerHr = 20;
+                int empHour = 8;
+                int salary = (empRatePerHr * empHour);
+                Console.WriteLine("Employee present for full day then salary is: " + salary);
+            }
+            else
+            {
+                int salary = 0;
+                Console.WriteLine("Employee is Absent then salary is:" + salary);
+            }
         }
     }
 }
